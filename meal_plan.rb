f32541e28d04e4bc7fb2396deb4b8b1bd72e5173
Pauline3414159 <fagie37@hotmail.com>
2021-06-08 15:44:42 -0400

    stuck on getting the view templates to render except for the layout

---
+++ meal_plan.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require "sinatra/content_for"
+require "tilt/erubis"
+require_relative "lib/week.rb"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+  set :erb, :escape_html => true
+end
+
+before do
+  
+  session[:week] = []
+  %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday).each do |day|
+    session[:week] << Day.new(day)
+  end
+end
+
+helpers do
+  def load_week
+    session[:week]
+  end
+end
+
+get "/" do
+  redirect "/index"
+end
+
+get "/index" do
+  @week = load_week
+  erb :index
+end
+
+get "/:day" do
+  @week = load_week
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  @current_day = @week[i_finder]
+  erb :day
+end
+
+post "/:day" do
+  
+end
+
+post "/:day/meal" do
+  
+end
