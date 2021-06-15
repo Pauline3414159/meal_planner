7e0995f117220cfc18544150f08b6e75a4a660ec
Pauline3414159 <fagie37@hotmail.com>
2021-06-15 13:20:46 -0400

    save before refactoring i_finder

--- meal_plan.rb
+++ meal_plan.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader' if development?
 require "sinatra/content_for"
 require "tilt/erubis"
 require 'psych'
-require_relative "lib/week.rb"
+require_relative "lib/day.rb"
 
 configure do
   enable :sessions
@@ -29,7 +29,52 @@ helpers do
     temp.courses = hsh[:courses]
     temp
   end
+
+  def save_week
+    @week.map do |d|
+      {day_of_week: d.day_of_week, meals: d.meals.map{|m| meal_parser(m)}}
+    end
+  end
 end
+
+# Create
+
+post "/:day/add_meal" do
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  begin
+    @week[i_finder].add(params[:type])
+  rescue => exception
+    session[:error] = exception.to_s
+    redirect back
+  else
+    File.open("data/working_week.yaml", "w") do |f|
+      f.write(save_week.to_yaml)
+    end
+    session[:success] = "#{params[:type]} was added to the meal plan"
+    redirect "/#{params[:day]}"
+  end  
+end
+
+post "/:day/:meal/add_course" do
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  @current_day = @week[i_finder]
+  meal_i_finder = @current_day.meals.index{ |m| m.type == params[:meal]}
+  @current_meal = @current_day.meals[meal_i_finder]
+  begin
+    @current_meal.add(params[:add_course])
+  rescue => exception
+    session[:error] = exception.to_s
+    redirect back
+  else
+    File.open("data/working_week.yaml", "w") do |f|
+      f.write(save_week.to_yaml)
+    end
+    session[:success] = "#{params[:add_course]} was added to #{params[:day]} #{params[:meal]}"
+    redirect "/#{@current_day.day_of_week}/#{@current_meal.type}"
+  end
+end
+
+# Read
 
 get "/" do
   redirect "/index"
@@ -43,22 +88,112 @@ end
 get "/:day" do
   i_finder = @week.index { |d| d.day_of_week == params[:day]}
   @current_day = @week[i_finder]
-  erb :day
+  if @current_day.meals.empty?
+    erb :empty_day
+  elsif @current_day.meals.size.between?(1,2)
+    erb :partial_day
+  elsif @current_day.meals.size >= 3
+    erb :full_day
+  end
 end
 
-post "/:day/add_meal" do
+get "/:day/:meal" do
   i_finder = @week.index { |d| d.day_of_week == params[:day]}
-  @week[i_finder].add(params[:type])
+  @current_day = @week[i_finder]
+  meal_i_finder = @current_day.meals.index{ |m| m.type == params[:meal]}
+  @current_meal = @current_day.meals[meal_i_finder]
+  if @current_meal.courses.empty?
+    erb :empty_meal
+  elsif @current_meal.courses.size.between?(1,2)
+    erb :partial_meal
+  elsif @current_meal.courses.size >= 3
+    erb :full_meal
+  end
+end
+
+# Update
+
+post "/:day/re_name_meal" do
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  begin
+    @week[i_finder].reclassify(params[:be_type], params[:re_type])
+  rescue => exception
+    session[:error] = exception.to_s
+    redirect back
+  else
+    File.open("data/working_week.yaml", "w") do |f|
+      f.write(save_week.to_yaml)
+    end
+    session[:success] = "#{params[:be_type]} was changed to #{params[:re_type]}"
+    redirect "/#{params[:day]}"
+  end
+  
+end
+
+post "/:day/:meal/rename_course" do
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  @current_day = @week[i_finder]
+  meal_i_finder = @current_day.meals.index{ |m| m.type == params[:meal]}
+  @current_meal = @current_day.meals[meal_i_finder]
+  begin
+    @current_meal.rename(params[:current_course], params[:new_name])
+  rescue => exception
+    session[:error] = exception.to_s
+    redirect back
+  else
+    File.open("data/working_week.yaml", "w") do |f|
+      f.write(save_week.to_yaml)
+    end
+    session[:success] = "#{params[:current_course]} was changed to #{params[:new_name]}"
+    redirect "/#{@current_day.day_of_week}/#{@current_meal.type}"
+  end
+end
+
+# Delete
+
+post "/:day/delete_meal" do
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  begin
+    @week[i_finder].remove(params[:del_type])
+  rescue => exception
+    session[:error] = exception.to_s
+    redirect back
+  else
+    File.open("data/working_week.yaml", "w") do |f|
+      f.write(save_week.to_yaml)
+    end
+    session[:success] = "#{params[:del_type]} was removed from the meal plan"
+    redirect "/#{params[:day]}"
+  end
+end
+
+post "/clear_all" do
   temp = @week.map do |d|
-    {day_of_week: d.day_of_week, meals: d.meals.map{|m| meal_parser(m)}}
+    {day_of_week: d.day_of_week, meals: []}
   end
   File.open("data/working_week.yaml", "w") do |f|
     f.write(temp.to_yaml)
   end
-  redirect "/#{params[:day]}"
+  session[:success] = "Everyting was deleted. Here's to a fresh start! :)"
+  redirect "/"
 end
 
-post "/:day/meal" do
-  
+post "/:day/:meal/delete_course" do
+  i_finder = @week.index { |d| d.day_of_week == params[:day]}
+  @current_day = @week[i_finder]
+  meal_i_finder = @current_day.meals.index{ |m| m.type == params[:meal]}
+  @current_meal = @current_day.meals[meal_i_finder]
+  begin
+    @current_meal.remove(params[:delete_course])
+  rescue => exception
+    session[:error] = exception.to_s
+    redirect back
+  else
+    File.open("data/working_week.yaml", "w") do |f|
+      f.write(save_week.to_yaml)
+    end
+    session[:success] = "#{params[:delete_course]} was removed from #{params[:day]} #{params[:meal]}"
+    redirect "/#{@current_day.day_of_week}/#{@current_meal.type}"
+  end
 end
 
